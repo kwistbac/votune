c8349e8ca3820afe1dcb9f6824c5d82f6f3b51a6
Padam <padam.shrest@gmail.com>
2013-11-27 22:33:05 +0200

    Error on currently playing song fixed. Currently, songs without queue value is also in playlist, discussion in team needed. Playlist updated every seconds.

--- mJuke/apps/establishment/playlist/views.py
+++ mJuke/apps/establishment/playlist/views.py
@@ -3,17 +3,9 @@ from django.template import Context, RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from mJuke.models import Song
 
-#def current(request):
-    #return render_to_response("establishment/playlist/current.html", {'song': song}, context_instance=RequestContext(request))
-
-#def next(request):
-    #return render_to_response("establishment/playlist/current.html", {'song': song}, context_instance=RequestContext(request))
 
 def playlist(request):
-    try:
-        songs = Song.objects.filter(account=request.user).exclude(queue=0).order_by('-queue')[:15]
-        curSong = Song.objects.get(queue=0, account=request.user)
-        return render_to_response("establishment/playlist/playlist.html", {'songs': songs, 'curSong': curSong},
-                                  context_instance=RequestContext(request))
-    except Song.DoesNotExist:
-        return HttpResponse('Songs were not found for this user.')
+    songs = Song.objects.filter(account=request.user).exclude(queue=0).order_by('-queue')[:15]
+    curSong = Song.objects.filter(queue=0, account=request.user)
+    return render_to_response("establishment/playlist/playlist.html", {'songs': songs, 'curSong': curSong},
+                              context_instance=RequestContext(request))
