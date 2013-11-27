1c7a660d922f6d2c92822ecd3b6abeaeeb840cf2
Kim Wistbacka <kim@kulturhuset.fi>
2013-11-27 17:07:03 +0200

    Update settings file

--- mJuke/settings.py
+++ mJuke/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mjuke', # Or path to database file if using sqlite3.
+        'NAME': 'mJuke', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'mJuke',
+        'PASSWORD': 'mJuke',
         'HOST': '127.0.0.1', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', # Set to empty string for default.
     }
