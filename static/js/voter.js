552f43df5d2117ed47724242e83c71b2bbf4a3f1
jsudket <jimsudket>
2014-02-06 06:34:01 +0200

    Voter fix
    Fixed bugs
    
    Cleaned abit; should add the other less importent stuff (about, help,
    suggest...) when the core voter functionality works 100%
    
    Todo:
    Having problem with showing search results in modal.
    cosmetic updates
    vote restriction

--- static/js/voter.js
+++ static/js/voter.js
@@ -16,19 +16,19 @@ $(document).ready(function()
 
         var nowPlaying = $('#nowplaying');
         nowPlaying.empty()
-        $('<span>').html(data.current.title + ' - ' + data.current.artist)
+        $('<span>').html('Now playing:' + data.current.title + ' - ' + data.current.artist)
                 .addClass('title')
         .appendTo(nowPlaying);
 
 
-        return updateQueue
+        return updateQueue(data)
     }
 
     function updateQueue(data) {
         if (!data.queue)
             return false;
 
-        var queue = $('#quaue');
+        var queue = $('#queue');
 
         queue.data('ts', new Date().getTime()).empty();
         $.each(data.queue, function (index, song) {
@@ -42,12 +42,16 @@ $(document).ready(function()
             $('<button>')
                  .addClass('btn btn-success btn-style')
                  .addClass('upvote-button')
+                 .addClass('pull-right')
+                 .html('<span class="glyphicon glyphicon-plus"></span>')
                  .attr({ type: 'button', id: song.id , value:'upvote'})
                  .appendTo(listItem)
 
              $('<button>')
-                 .addClass('btn btn-success btn-style')
+                 .addClass('btn btn-danger btn-style')
                  .addClass('downvote-button')
+                 .addClass('pull-right')
+                 .html('<span class="glyphicon glyphicon-minus"></span>')
                  .attr({ type: 'button', id: song.id , value:'downvote'})
                  .appendTo(listItem)
 
@@ -92,5 +96,44 @@ $(document).ready(function()
             'success': updateQueue
         });
      });
-}
-);
+
+    $(document).on('click', ".downvote-button", function (e) {
+        e.preventDefault();
+
+        jQuery.ajax(
+        {
+            'type': 'POST',
+            'url': 'downvote/',
+            'data': { 'songId': $(this).attr('id')},
+            'dataType': 'json',
+            'success': updateQueue
+        });
+     });
+
+     $('#SearchForm').submit(function() {
+        jQuery.ajax(
+        {
+            type: $(this).attr('method'),
+            'url': $(this).attr('action'),
+            'data': $(this).serialize(),
+            'success': showResults
+        });
+
+     });
+
+    function showResults(data)
+    {
+
+        var modal = $('<div>').attr('id', 'searchResultModal')
+                .addClass('modal')
+                .addClass('fade');
+
+        modal.on('hidden.bs.modal', function () {
+             $(this).remove();
+        })
+        .modal()
+        data.trigger('loaded.bs.modal');
+
+
+    }
+});
